b30d764ae8b21a1e1351214a9373723da811acdb
Farhan Ghiffari <redfield71421@gmail.com>
2024-10-12 01:12:46 +0700

    app modul

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Red651/app-say-hello
+
+go 1.23.1
+
+require github.com/Red651/golang-belajar-modul v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	Sayhello "github.com/Red651/golang-belajar-modul"
+)
+
+func main() {
+	fmt.Println(Sayhello.Sayhello("Farhan"))
+}
